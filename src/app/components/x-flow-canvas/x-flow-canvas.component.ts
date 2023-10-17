11f3bb13296526ef60e9a57437b6fcab9d584066
李豪珣 <519367854@qq.com>
2023-10-17 23:37:18 +0800

    feat: 添加flow工作台和canvas组件

--- src/app/components/x-flow-canvas/x-flow-canvas.component.ts
+++ src/app/components/x-flow-canvas/x-flow-canvas.component.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-x-flow-canvas',
   templateUrl: './x-flow-canvas.component.html',
-  styleUrls: ['./x-flow-canvas.component.less']
+  styleUrls: ['./x-flow-canvas.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class XFlowCanvasComponent {
   @ViewChild('rootRef') rootRef: ElementRef | undefined;
