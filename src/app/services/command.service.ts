708b52281d1dc801f53cbc006091b865ec869f23
李豪珣 <519367854@qq.com>
2023-10-21 22:47:17 +0800

    feat: 添加demo假数据

--- src/app/services/command.service.ts
+++ src/app/services/command.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { IGraphCommandService } from '@/app/interfaces/graph-command.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CommandService {
+export class CommandService implements IGraphCommandService {
+  // constructor() { }
 
-  constructor() { }
+  executeCommand<Args, Result>(commandId: string, args: Args, hooks: any): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+
+  redoCommand(): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+
+  redoable: boolean;
+
+  undoCommand(): Promise<void> {
+    return Promise.resolve(undefined);
+  }
+
+  undoable: boolean;
+  watchChange: any;
 }
