1c7b124c88f54f9bdab0a1348c2e036332f8f094
李豪珣 <519367854@qq.com>
2023-10-31 23:37:15 +0800

    feat: 引入上下文

--- src/app/commands/graph/graph-render.ts
+++ src/app/commands/graph/graph-render.ts
@@ -1,9 +1,12 @@
 import { XFlowGraphCommands } from '@/app/constants';
 import { Graph } from '@antv/x6';
+import { CmdContext } from '@/app/commands';
 
 export class GraphRenderCommand {
   token: string = XFlowGraphCommands.GRAPH_RENDER.id;
 
+  // 需要一个上下文
+  ctx: CmdContext;
   // TODO 进行测试
   args;
   graph: Graph;
