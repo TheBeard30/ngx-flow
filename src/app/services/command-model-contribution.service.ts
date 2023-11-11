e20f3c99f418c19d04d4df508167e18066cb7dd7
李豪珣 <519367854@qq.com>
2023-11-11 22:12:28 +0800

    feat: 添加graph model的配置

--- src/app/services/command-model-contribution.service.ts
+++ src/app/services/command-model-contribution.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CommandService } from '@/app/services/command.service';
+import { IModelService } from '@/app/interfaces/model.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommandModelContributionService {
   constructor(private command: CommandService) {}
+
+  registerModel(registry: IModelService) {}
 }
--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { IModelOptions, IModelService, NsModel, Token } from '@/app/interfaces/model.interface';
+import { CommandModelContributionService } from '@/app/services/command-model-contribution.service';
+import { GraphModelContribution } from '@/app/services/graph-model-contribution.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModelService implements IModelService {
-  constructor() {}
+  constructor(
+    private commandModelContribution: CommandModelContributionService,
+    private graphModelContribution: GraphModelContribution
+  ) {}
+
+  onStart() {
+    this.commandModelContribution.registerModel(this);
+    this.graphModelContribution.registerModel(this);
+  }
 
   registerModel<T>(options: IModelOptions<T>): void {}
 
