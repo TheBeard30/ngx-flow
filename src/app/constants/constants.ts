d535dbcf72d968585d8142824b9ac851d9b34787
李豪珣 <519367854@qq.com>
2023-11-12 22:13:00 +0800

    fix: 修改常量

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -18,3 +18,28 @@ export const XFLOW_DEFAULT_GROUP_NODE = 'XFLOW_DEFAULT_GROUP_NODE';
 
 /** edge render 的默认key值 */
 export const XFLOW_DEFAULT_EDGE = 'XFLOW_DEFAULT_EDGE';
+
+/** 扩展事件:事件参数 */
+export interface XFlowEventRecord {
+  TOGGLE_MULTI_SELECT: {
+    isEnable: boolean;
+  };
+}
+
+/** 事件名: 扩展事件 */
+export type XFlowEventName = keyof XFlowEventRecord;
+
+/** localStorage key */
+export const LOCAL_STORAGE_KEY = 'XFLOW_COPY_ITEMS';
+
+/** node height */
+export const DEFAULT_NODE_HEIGHT = 36;
+
+/** node width */
+export const DEFAULT_NODE_WIDTH = 180;
+
+/** default group size */
+export const XFLOW_GROUP_DEFAULT_COLLAPSED_SIZE = {
+  width: DEFAULT_NODE_WIDTH,
+  height: DEFAULT_NODE_HEIGHT
+};
