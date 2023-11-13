10ffae9c510312d36fffa4fb3392c1232f57a3d4
李豪珣 <519367854@qq.com>
2023-11-13 22:46:07 +0800

    perf:添加工具方法

--- src/app/models/application.model.ts
+++ src/app/models/application.model.ts
@@ -1,7 +1,7 @@
-import { CommandInjectionToken, IGraphCommandService, IGraphProvider } from '@/app/interfaces';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CommandService, GraphProviderService, ModelService } from '@/app/services';
 import { CommandContributionService } from '@/app/services/command-contribution.service';
+import { cellsToJson } from '@/app/utils/app.util';
 
 @Injectable({ providedIn: 'root' })
 export class Application {
@@ -12,8 +12,62 @@ export class Application {
     public commandContributionService: CommandContributionService
   ) {}
 
+  /**
+   * 启动app
+   */
   start() {
     // TODO 启动配置
     this.commandContributionService.start();
   }
+
+  /**
+   * 获取画布实例
+   */
+  public getGraphInstance() {
+    return this.graphProvider.getGraphInstance();
+  }
+
+  /**
+   * 获取画布配置项
+   */
+  public getGraphConfig() {
+    return this.graphProvider.getGraphOptions();
+  }
+
+  /**
+   * 获取画布配置项
+   */
+  public getGraphData = async () => {
+    const graph = await this.graphProvider.getGraphInstance();
+    const cells = graph.getCells();
+    return cellsToJson(cells);
+  };
+
+  /**
+   * 获取画布所有节点
+   */
+  public getAllNodes = async () => {
+    const graph = await this.graphProvider.getGraphInstance();
+    return graph.getNodes();
+  };
+
+  /**
+   * 获取画布节点
+   */
+  public getNodeById = async (nodeId: string) => {
+    const graph = await this.graphProvider.getGraphInstance();
+    return graph.getCellById(nodeId);
+  };
+
+  /** 获取画布所有连线 */
+  public getAllEdges = async () => {
+    const graph = await this.graphProvider.getGraphInstance();
+    return graph.getEdges();
+  };
+
+  /** 获取画布连线 */
+  public getEdgeById = async (edgeId: string) => {
+    const graph = await this.graphProvider.getGraphInstance();
+    return graph.getCellById(edgeId);
+  };
 }
--- src/app/utils/app.util.ts
+++ src/app/utils/app.util.ts
@@ -1,6 +1,65 @@
 import { Application } from '@/app/models';
 import { Injector } from '@angular/core';
+import { Cell, Edge, Node } from '@antv/x6';
+import { IEdgeConfig, INodeConfig } from '@/app/interfaces';
 
 export const initApp = (injector: Injector) => {
   return injector.get(Application);
+};
+
+export const node2Json = (cell: Node) => {
+  const children = cell.getChildren();
+  const size = cell.getSize();
+  const data = cell.getData<INodeConfig>() || ({} as INodeConfig);
+  const position = cell.getPosition();
+  const groupId = cell.getParentId();
+  const { isCollapsed } = data;
+  const groupCollapsedSize = data.groupCollapsedSize || (isCollapsed ? size : null);
+  return {
+    ...data,
+    id: cell.id,
+    width: size.width,
+    height: size.height,
+    x: position.x,
+    y: position.y,
+    group: groupId,
+    groupChildren: children ? children.map(child => child.id) : null,
+    groupCollapsedSize
+  };
+};
+
+export const edge2Json = (cell: Edge) => {
+  const data = cell.getData<any>() || {};
+  return {
+    ...data,
+    id: cell.id,
+    source: cell.getSourceCellId(),
+    target: cell.getTargetCellId(),
+    sourcePortId: cell.getSourcePortId(),
+    targetPortId: cell.getTargetPortId(),
+    sourcePort: cell.getSourcePortId(),
+    targetPort: cell.getTargetPortId()
+  };
+};
+
+export const cellsToJson = (cells: Cell<Cell.Properties>[]) => {
+  const nodes: INodeConfig[] = [];
+  const edges: IEdgeConfig[] = [];
+
+  const cell2Json = (cell: Cell) => {
+    if (cell.isNode()) {
+      nodes.push(node2Json(cell));
+    }
+    if (cell.isEdge()) {
+      edges.push(edge2Json(cell));
+    }
+  };
+  cells.map(cell => {
+    return cell2Json(cell);
+  });
+
+  return {
+    nodes,
+    edges
+  };
 };
