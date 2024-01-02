194e90c29cc5dd78877a10f2444b7ba007b9fd0f
李豪珣 <519367854@qq.com>
2024-01-02 23:32:05 +0800

    feat: 添加node，edge节点组件

---
+++ src/app/flow-extension/editor-panel/components/edge.widget.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edge-widget',
+  template: ``,
+  styles: [``],
+  standalone: true
+})
+export class EdgeWidget {}
---
+++ src/app/flow-extension/editor-panel/components/node.widget.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-node-widget',
+  template: ``,
+  styles: [``],
+  standalone: true
+})
+export class NodeWidget {}
