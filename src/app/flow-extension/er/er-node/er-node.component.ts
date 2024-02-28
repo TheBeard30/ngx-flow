1238bb38679749fcc8bbf20ad07006f6dc7f51df
王航 <1312154923@qq.com>
2024-02-28 18:05:33 +0800

    优化er-node，添加连接桩

--- src/app/flow-extension/er/er-node/er-node.component.ts
+++ src/app/flow-extension/er/er-node/er-node.component.ts
@@ -1,6 +1,7 @@
 import { XFlowNodeCommands } from '@/app/flow-core/constants';
 import { CommandService, GraphProviderService } from '@/app/flow-core/services';
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { getNodePorts } from '../utils';
 
 @Component({
   selector: 'app-er-node',
@@ -10,11 +11,14 @@ import { AfterViewInit, Component, Input } from '@angular/core';
 export class ErNodeComponent {
   @Input() entity: any;
   @Input() id: string;
-  constructor(private graphProvider: GraphProviderService, private commandService: CommandService) {
-    graphProvider.getGraphInstance().then(g => {
-      const self = g.getCellById(this.id);
-    });
-  }
+  //是否展示所有字段
+  expand = false;
+  //隐藏掉的连线
+  hiddenEdges = []
+
+
+  constructor(private graphProvider: GraphProviderService, private commandService: CommandService) { }
+
   getCls() {
 
     if (this.entity?.entityType === 'FACT') {
@@ -34,5 +38,72 @@ export class ErNodeComponent {
         id: this.id
       }
     });
+  }
+
+  //展开所有字段
+  expandField() {
+    this.graphProvider.getGraphInstance().then(g => {
+      const self = g.getCellById(this.id);
+      //获取当前节点连线
+      const edges = g.getConnectedEdges(self);
+      //将隐藏的连线显示
+      if (this.hiddenEdges.length > 0) {
+        edges.push(...this.hiddenEdges)
+      }
+      if (self.isNode()) {
+        //根据字段数量扩展节点大小
+        self.size(170, 200 + (this.entity.entityProperties.length - 5) * 28)
+        //删除以前的连接桩
+        self.removePorts();
+        //根据当前字段建立新的连接桩
+        self.addPorts(getNodePorts(this.entity.entityProperties).items);
+        //恢复连线
+        g.addEdges(edges);
+      }
+    });
+
+    this.expand = true;
+  }
+
+  //收起字段
+  hiddenField() {
+    this.graphProvider.getGraphInstance().then(g => {
+      const self = g.getCellById(this.id);
+      //当前节点所有连线（分为出和入两种）
+      const incomingEdges = g.getIncomingEdges(self);
+      const outgoingEdges = g.getOutgoingEdges(self);
+      const edges = [];
+      //判断节点incoming的连线是否隐藏
+      if (incomingEdges != undefined && incomingEdges.length > 0) {
+        incomingEdges.forEach(e => {
+          if (parseInt(e.getTargetPortId().slice(-1), 10) > 5) {
+            this.hiddenEdges.push(e);
+          } else {
+            edges.push(e);
+          }
+        });
+      }
+      //判断节点outgoing的连线是否隐藏
+      if (outgoingEdges != undefined && outgoingEdges.length > 0) {
+        outgoingEdges.forEach(e => {
+          if (parseInt(e.getSourcePortId().slice(-1), 10) > 5) {
+            this.hiddenEdges.push(e);
+          } else {
+            edges.push(e);
+          }
+        });
+      }
+      if (self.isNode()) {
+        //根据字段数量扩展节点大小
+        self.size(170, 200)
+        //删除以前的连接桩
+        self.removePorts();
+        //根据当前字段建立新的连接桩
+        self.addPorts(getNodePorts(this.entity.entityProperties.slice(0, 5)).items);
+        //恢复连线
+        g.addEdges(edges);
+      }
+    });
+    this.expand = false;
   }
 }
--- src/app/flow-extension/er/er-table-panel/er-table-panel.component.ts
+++ src/app/flow-extension/er/er-table-panel/er-table-panel.component.ts
@@ -1,18 +1,18 @@
 import { IPosition } from '@/app/flow-core/interfaces';
 import { CommandService, GraphProviderService } from '@/app/flow-core/services';
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Graph, Node } from '@antv/x6';
 import { Dnd } from '@antv/x6-plugin-dnd';
 import ValidateNodeOptions = Dnd.ValidateNodeOptions;
 import Properties = Node.Properties;
-import { uuidv4 } from '@/app/flow-core/models';
+import { getNodePorts, registerPortLayout } from '../utils';
 
 @Component({
   selector: 'app-er-table-panel',
   templateUrl: './er-table-panel.component.html',
   styleUrls: ['./er-table-panel.component.less']
 })
-export class ErTablePanelComponent implements AfterViewInit {
+export class ErTablePanelComponent implements OnInit, AfterViewInit {
   @Input() position: IPosition = { width: 240, top: 40, bottom: 0, left: 0 };
   @Input() table: any[];
 
@@ -33,6 +33,9 @@ export class ErTablePanelComponent implements AfterViewInit {
 
   constructor(private graphProvider: GraphProviderService, private commandService: CommandService) { }
 
+  ngOnInit(): void {
+    registerPortLayout()
+  }
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.createDnd();
@@ -60,19 +63,21 @@ export class ErTablePanelComponent implements AfterViewInit {
   }
 
   startDrag(e: MouseEvent, item: any) {
+    console.log('item', item)
     // 该 node 为拖拽的节点，默认也是放置到画布上的节点，可以自定义任何属性
     const node = this.graph.createNode({
       shape: 'er-node',
+      ports: getNodePorts(item.entityProperties.slice(0, 5))
     })
     node.setData({
       ngArguments: {
         entity: item,
-        id: node.id
+        id: node.id,
       }
     });
-
     this.dnd.start(node, e)
   }
+
 
   setCollapse() {
     this.collapse = !this.collapse;
