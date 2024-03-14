626eb5fa78e997acec3d673621c828d2dd10548e
王航 <1312154923@qq.com>
2024-03-14 16:59:38 +0800

    优化er图toolbar

--- src/app/pages/er/er.component.ts
+++ src/app/pages/er/er.component.ts
@@ -1,10 +1,5 @@
 import { GraphProviderService } from '@/app/flow-core/services';
-import { getPorts } from '@/app/flow-extension/flow-chart/flow-node-panel/utils';
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, ViewChild } from '@angular/core';
-import { MiniMap } from '@antv/x6-plugin-minimap';
-import { Scroller } from '@antv/x6-plugin-scroller';
-import { Snapline } from '@antv/x6-plugin-snapline';
-import { SimpleNodeView } from '../../flow-extension/minimap/simple-node-view/simple-node-view.component';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-er',
@@ -14,12 +9,7 @@ import { SimpleNodeView } from '../../flow-extension/minimap/simple-node-view/si
 export class ErComponent implements AfterViewInit {
   graphData;
   graphStatus = 'NORMAL';
-  /** 鼠标的引用 */
-  @ViewChild('cursorTipRef') cursorTipRef: ElementRef;
-  //添加节点位置
-  cursorLocation: any;
-  //是否显示CreateNodeModal
-  IsShowCreateNodeModal = false;
+
   //暂时将数据写死
   entity: any = {
     entityName: '测试表',
@@ -316,71 +306,6 @@ export class ErComponent implements AfterViewInit {
     console.log('flow app>>>', app);
   };
 
-  onclick() {
-    this.graphProvider.getGraphInstance().then(g => {
-      //删除transfrom插件
-      g.disablePlugins('transform');
-      //创建er-node节点
-      const node = g.createNode({
-        shape: "er-node",
-        position: { x: this.cursorLocation.x, y: this.cursorLocation.y - 40 },
-        ports: getPorts()
-      });
-      node.setData({
-        ngArguments: {
-          entity: this.entity,
-          id: node.id
-        }
-      })
-      g.addNode({
-        position: { x: this.cursorLocation.x, y: this.cursorLocation.y - 40 },
-        size: { width: 170, height: 200 },
-        markup: [
-          { tagName: 'rect', selector: 'body' },
-          { tagName: 'text', selector: 'lable', textContent: '123' }
-        ]
-      })
-    });
-  }
-  //改变建立节点提示框状态
-  configCursorTip({ left, top, display }) {
-    this.cursorTipRef.nativeElement.style.left = left;
-    this.cursorTipRef.nativeElement.style.top = top;
-    this.cursorTipRef.nativeElement.style.display = display;
-  }
-  //创建节点过程中鼠标事件监听
-  mouseMove(e) {
-    if (this.graphStatus === 'CREATE') {
-      this.configCursorTip({
-        left: `${e.pageX}px`,
-        top: `${e.pageY}px`,
-        display: 'block',
-      });
-    }
-  }
-  mouseDown(e) {
-    if (this.graphStatus === 'CREATE') {
-      console.log('mouseDown')
-      this.cursorLocation = { x: e.pageX, y: e.pageY }
-      this.IsShowCreateNodeModal = true;
-      this.onclick()
-      this.configCursorTip({
-        left: '0px',
-        top: '0px',
-        display: 'none',
-      })
-      this.changeGraphStatus('NORMAL')
-    }
-  }
-  mouseLeave(e) {
-    if (this.graphStatus === 'CREATE') {
-      this.configCursorTip({
-        left: '0px',
-        top: '0px',
-        display: 'none',
-      })
-    }
-  }
 
   changeGraphStatus(status: string) {
     this.graphStatus = status;
