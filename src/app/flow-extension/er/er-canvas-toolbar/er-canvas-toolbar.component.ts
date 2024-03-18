40d2a6852f29670cfcaeb1bbe17b8802c40ebfc1
王航 <1312154923@qq.com>
2024-03-18 12:14:17 +0800

    优化er图toolbar

--- src/app/flow-extension/er/er-canvas-toolbar/er-canvas-toolbar.component.ts
+++ src/app/flow-extension/er/er-canvas-toolbar/er-canvas-toolbar.component.ts
@@ -4,7 +4,8 @@ import { Edge, Node } from '@antv/x6';
 import * as MODELS from '@/app/flow-core/constants/model-constant';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { CommandService } from '@/app/flow-core/services';
-import { XFlowEdgeCommands, XFlowNodeCommands } from '@/app/flow-core/constants';
+import { XFlowEdgeCommands, XFlowGraphCommands, XFlowNodeCommands } from '@/app/flow-core/constants';
+import { NsGraphFullscreen, NsGraphZoom } from '@/app/flow-core/commands';
 
 @Component({
   selector: 'app-er-canvas-toolbar',
@@ -21,6 +22,9 @@ export class ErCanvasToolbarComponent implements OnInit {
   @Input() dataSource: string;
   @Input() dataSourceMenu: any;
   @Output() changeDataSource = new EventEmitter<string>;
+
+  //画布是否全屏
+  fullScreen = false;
 
   constructor(private app: Application, private commandService: CommandService, private message: NzMessageService) { }
 
@@ -39,8 +43,25 @@ export class ErCanvasToolbarComponent implements OnInit {
           this.selectedEdge = cells.filter(cell => cell.isEdge()) as Edge[];
         })
       });
+      //监听画布是否全屏
+      MODELS.GRAPH_FULLSCREEN.getModel(modelService).then(model => {
+        /** 初始化全屏默认值 */
+        model.setValue(false);
+        model.watch(fullscreen => {
+          this.fullScreen = fullscreen;
+        })
+      })
     }
   }
+  //画布操作
+  zoom(factor: number | "fit" | "real") {
+    this.commandService.executeCommand<NsGraphZoom.IArgs, NsGraphZoom.IResult>(XFlowGraphCommands.GRAPH_ZOOM.id, {
+      factor: factor,
+      zoomOptions: { maxScale: 1.5, minScale: 0.05 }
+    });
+    return null;
+  }
+
   //删除节点按钮样式交互
   isDisable(type: string): string {
     if (type === 'node') {
@@ -70,4 +91,9 @@ export class ErCanvasToolbarComponent implements OnInit {
       });
     });
   }
+  //切换全屏
+  toggleFullScreen() {
+    this.commandService.executeCommand<NsGraphFullscreen.IArgs, NsGraphFullscreen.IResult>(XFlowGraphCommands.GRAPH_FULLSCREEN.id, {})
+  }
+
 }
